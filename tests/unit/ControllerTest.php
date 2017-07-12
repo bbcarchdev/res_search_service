4202c9b75c01d752c77993eddef38d3c1b6cc497
Elliot Smith <elliot.smith@bbc.co.uk>
2017-07-12 11:59:51 +0100

    Add unit tests for remaining Controller methods

--- tests/unit/ControllerTest.php
+++ tests/unit/ControllerTest.php
@@ -35,6 +35,26 @@ final class ControllerTest extends TestCase
         'audiences' => '/audiences'
     );
 
+    public function testMinimal()
+    {
+        // we expect this capabilities string to be encoded into the HTML page
+        $expectedCapabilitiesString = json_encode($this->capabilities);
+
+        $environment = Environment::mock(array(
+            'REQUEST_METHOD' => 'GET',
+            'REQUEST_URI' => '/minimal'
+        ));
+        $request = Request::createFromEnvironment($environment);
+        $response = new Response();
+
+        $client = $this->getMockBuilder(RESClient::class)->getMock();
+        $controller = new Controller($client, $this->capabilities);
+
+        $response = $controller->minimal($request, $response);
+        $body = (string)$response->getBody();
+        $this->assertContains($expectedCapabilitiesString, $body);
+    }
+
     public function testAudiences()
     {
         $audiences = array(
@@ -67,6 +87,84 @@ final class ControllerTest extends TestCase
 
         $response = $controller->audiences($request, $response);
         $body = (string)$response->getBody();
-        $this->assertSame(json_encode($audiences, TRUE), $body);
+        $this->assertJsonStringEqualsJsonString(json_encode($audiences, TRUE), $body);
+    }
+
+    public function testProxy()
+    {
+        $topicUri = 'http://foo.bar/topic1';
+
+        $proxyBody = array(
+            'acropolis_uri' => $topicUri
+        );
+
+        $client = $this->getMockBuilder(RESClient::class)
+                       ->setMethods(['proxy'])
+                       ->getMock();
+
+        $client->expects($this->once())
+               ->method('proxy')
+               ->with($topicUri, 'video')
+               ->willReturn($proxyBody);
+
+        $environment = Environment::mock(array(
+            'REQUEST_METHOD' => 'GET',
+            'REQUEST_URI' => '/proxy?media=video&uri=' . $topicUri
+        ));
+        $request = Request::createFromEnvironment($environment);
+        $response = new Response();
+
+        $controller = new Controller($client, $this->capabilities);
+
+        $response = $controller->proxy($request, $response);
+        $body = (string)$response->getBody();
+        $this->assertJsonStringEqualsJsonString(json_encode($proxyBody, TRUE), $body);
+    }
+
+    public function testSearch()
+    {
+        $searchBody = array(
+            'items' => array(
+                array(
+                    'topic_uri' => 'http://foo.bar/topic1'
+                )
+            )
+        );
+
+        $query = 'tench';
+        $media = 'video';
+        $limit = 5;
+        $offset = 10;
+        $audiences = array('http://foo.bar/audience1', 'http://foo.bar/audience2');
+
+        $client = $this->getMockBuilder(RESClient::class)
+                       ->setMethods(['search'])
+                       ->getMock();
+
+        $client->expects($this->once())
+               ->method('search')
+               ->with($query, $media, $limit, $offset, $audiences)
+               ->willReturn($searchBody);
+
+        $qstring = "?q=$query&media=$media&limit=$limit&offset=$offset&for[]=" .
+                   implode($audiences, '&for[]=');
+
+        $environment = Environment::mock(array(
+            'REQUEST_METHOD' => 'GET',
+            'REQUEST_URI' => '/search' . $qstring
+        ));
+        $request = Request::createFromEnvironment($environment);
+        $response = new Response();
+
+        $controller = new Controller($client, $this->capabilities);
+
+        $response = $controller->search($request, $response);
+
+        $body = json_decode($response->getBody(), TRUE);
+        $item = $body['items'][0];
+
+        $expectedApiUri = 'http://localhost/proxy?uri=http://foo.bar/topic1&media=video';
+        $actualApiUri = $item['api_uri'];
+        $this->assertEquals($expectedApiUri, $actualApiUri);
     }
 }
